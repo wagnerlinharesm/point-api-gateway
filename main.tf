c0623675fb3a1a7934e3e1cddb1a2be25fb61978
wagnerlm <wagnerlink@gmail.com>
2024-03-19 16:39:34 -0300

    fix

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# -- ------------------------------------------------------
+# -- gateway
+# -- ------------------------------------------------------
+
 data "aws_lambda_function" "point_lambda_authorizer" {
   function_name = "point_lambda_authorizer"
 }
@@ -17,6 +21,35 @@ resource "aws_api_gateway_request_validator" "validator" {
   validate_request_parameters = true
 }
 
+resource "aws_api_gateway_authorizer" "cognito_authorizer" {
+  name            = "cognito-authorizer"
+  rest_api_id     = aws_api_gateway_rest_api.point_api_gateway.id
+  type            = "COGNITO_USER_POOLS"
+  identity_source = "method.request.header.Authorization"
+  provider_arns   = ["arn:aws:cognito-idp:us-east-2:644237782704:userpool/us-east-2_3HJlRalTj"]
+}
+
+resource "aws_api_gateway_deployment" "point-api-gateway-deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    ]
+  rest_api_id = aws_api_gateway_rest_api.point_api_gateway.id
+  stage_name  = "dev"
+}
+
+resource "aws_lambda_permission" "point_lambda_authorizer_permission" {
+  statement_id  = "AllowAPIInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.point_lambda_authorizer.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.point_api_gateway.execution_arn}/*"
+}
+
+# -- ------------------------------------------------------
+# -- route auth
+# -- ------------------------------------------------------
+
 resource "aws_api_gateway_resource" "auth_resource" {
   rest_api_id = aws_api_gateway_rest_api.point_api_gateway.id
   parent_id   = aws_api_gateway_rest_api.point_api_gateway.root_resource_id
@@ -30,7 +63,6 @@ resource "aws_api_gateway_method" "auth_method" {
   authorization = "NONE"
 }
 
-
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.point_api_gateway.id
   resource_id             = aws_api_gateway_resource.auth_resource.id
@@ -38,22 +70,4 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:644237782704:function:point_lambda_authorizer/invocations"
-}
-
-
-resource "aws_api_gateway_authorizer" "cognito_authorizer" {
-  name            = "cognito-authorizer"
-  rest_api_id     = aws_api_gateway_rest_api.point_api_gateway.id
-  type            = "COGNITO_USER_POOLS"
-  identity_source = "method.request.header.Authorization"
-  provider_arns   = ["arn:aws:cognito-idp:us-east-2:644237782704:userpool/us-east-2_3HJlRalTj"]
-}
- 
-resource "aws_lambda_permission" "point_lambda_authorizer_permission" {
-  statement_id  = "AllowAPIInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.point_lambda_authorizer.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.point_api_gateway.execution_arn}/*"
 }
