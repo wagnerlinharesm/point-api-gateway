3e632b30a0f0fa5a457a737f263bfc08b58b3473
wagnerlm <wagnerlink@gmail.com>
2024-03-20 13:39:45 -0300

    add query route

--- main.tf
+++ main.tf
@@ -149,3 +149,34 @@ resource "aws_api_gateway_integration_response" "report_generate_integration_res
     aws_api_gateway_integration.report_generate_integration
   ]
 }
+
+# -- ------------------------------------------------------
+# -- query route
+# -- ------------------------------------------------------
+
+data "aws_lambda_function" "point_query" {
+  function_name = "point_query"
+}
+
+resource "aws_api_gateway_resource" "auth_resource" {
+  rest_api_id = aws_api_gateway_rest_api.point_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.point_api_gateway.root_resource_id
+  path_part   = "query"
+}
+
+resource "aws_api_gateway_method" "query_method" {
+  rest_api_id   = aws_api_gateway_rest_api.point_api_gateway.id
+  resource_id   = aws_api_gateway_resource.auth_resource.id
+  http_method   = "POST"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "point_query_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.point_api_gateway.id
+  resource_id             = aws_api_gateway_resource.auth_resource.id
+  http_method             = aws_api_gateway_method.auth_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:644237782704:function:point_query/invocations"
+}
