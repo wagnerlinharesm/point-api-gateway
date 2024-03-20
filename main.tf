a111f0d90ad9d61f569ae011802912ed08091889
GAPrado <guilhermeprado@outlook.com>
2024-03-19 21:51:20 -0300

    add report generate

--- main.tf
+++ main.tf
@@ -71,3 +71,72 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:644237782704:function:point_lambda_authorizer/invocations"
 }
+
+# -- ------------------------------------------------------
+# -- report route
+# -- ------------------------------------------------------
+
+data "aws_sqs_queue" "point_report_sqs_queue" {
+  name = var.point_report_sqs_queue_name
+}
+
+resource "aws_api_gateway_resource" "report_resource" {
+  rest_api_id = aws_api_gateway_rest_api.point_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.point_api_gateway.root_resource_id
+  path_part   = "report"
+}
+
+resource "aws_api_gateway_resource" "report_generate_resource" {
+  rest_api_id = aws_api_gateway_rest_api.point_api_gateway.id
+  parent_id   = aws_api_gateway_resource.report_resource.id
+  path_part   = "generate"
+}
+
+resource "aws_api_gateway_method" "report_generate_post_method" {
+  rest_api_id           = aws_api_gateway_rest_api.point_api_gateway.id
+  resource_id           = aws_api_gateway_resource.report_generate_resource.id
+  http_method           = "POST"
+  authorization         = "COGNITO_USER_POOLS"
+  authorizer_id         = aws_api_gateway_authorizer.cognito_authorizer.id
+
+  request_models        = {
+    "application/json" = "Empty"
+  }
+
+  request_validator_id  = aws_api_gateway_request_validator.validator.id
+}
+
+resource "aws_api_gateway_method_response" "report_generate_post_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.point_api_gateway.id
+  resource_id = aws_api_gateway_resource.report_generate_resource.id
+  http_method = aws_api_gateway_method.report_generate_post_method.http_method
+  status_code = "201"
+}
+
+resource "aws_api_gateway_integration" "report_generate_integration" {
+  rest_api_id               = aws_api_gateway_rest_api.point_api_gateway.id
+  resource_id               = aws_api_gateway_resource.report_generate_resource.id
+  http_method               = aws_api_gateway_method.report_generate_post_method.http_method
+
+  integration_http_method   = "POST"
+  type                      = "AWS"
+
+  uri                       = data.aws_sqs_queue.point_report_sqs_queue.arn
+
+  request_templates = {
+    "application/json" = <<EOF
+    {
+      "Authorization": "$util.escapeJavaScript($input.params().header.get('Authorization'))",
+    }
+    EOF
+  }
+
+  passthrough_behavior = "NEVER"
+}
+
+resource "aws_api_gateway_integration_response" "report_generate_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.point_api_gateway.id
+  resource_id = aws_api_gateway_resource.report_generate_resource.id
+  http_method = aws_api_gateway_method.report_generate_post_method.http_method
+  status_code = aws_api_gateway_method_response.report_generate_post_method_response.status_code
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "point_report_sqs_queue_name" {
+  type    = string
+  default = "point_report"
+}
