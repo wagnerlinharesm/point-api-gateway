cb18eca1ef5ce24646edfeb7b24a16dc446a6969
lucas vieira <lucas.vss12@gmail.com>
2024-03-20 21:11:32 -0300

    Adiciona rota para point

--- main.tf
+++ main.tf
@@ -154,31 +154,61 @@ resource "aws_api_gateway_integration_response" "report_generate_integration_res
 # -- query route
 # -- ------------------------------------------------------
 
+data "aws_lambda_function" "point_save" {
+  function_name = "point_save"
+}
+
 data "aws_lambda_function" "point_query" {
   function_name = "point_query"
 }
 
-resource "aws_api_gateway_resource" "query_resource" {
+resource "aws_api_gateway_resource" "point_resource" {
   rest_api_id = aws_api_gateway_rest_api.point_api_gateway.id
   parent_id   = aws_api_gateway_rest_api.point_api_gateway.root_resource_id
-  path_part   = "query"
+  path_part   = "point"
 }
 
-resource "aws_api_gateway_method" "query_method" {
+resource "aws_api_gateway_method" "save_method" {
   rest_api_id   = aws_api_gateway_rest_api.point_api_gateway.id
-  resource_id   = aws_api_gateway_resource.query_resource.id
+  resource_id   = aws_api_gateway_resource.point_resource.id
   http_method   = "POST"
   authorization = "COGNITO_USER_POOLS"
   authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
 }
 
-resource "aws_api_gateway_integration" "point_query_lambda_integration" {
+resource "aws_api_gateway_method" "query_method" {
+  rest_api_id   = aws_api_gateway_rest_api.point_api_gateway.id
+  resource_id   = aws_api_gateway_resource.point_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "point_save_lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.point_api_gateway.id
-  resource_id             = aws_api_gateway_resource.query_resource.id
-  http_method             = aws_api_gateway_method.query_method.http_method
+  resource_id             = aws_api_gateway_resource.point_resource.id
+  http_method             = aws_api_gateway_method.save_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:644237782704:function:point_save/invocations"
+}
+
+resource "aws_api_gateway_integration" "point_query_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.point_api_gateway.id
+  resource_id             = aws_api_gateway_resource.point_resource.id
+  http_method             = aws_api_gateway_method.query_method.http_method
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:644237782704:function:point_query/invocations"
+}
+
+resource "aws_lambda_permission" "point_save_permission" {
+  statement_id  = "AllowAPIInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.point_save.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.point_api_gateway.execution_arn}/*"
 }
 
 resource "aws_lambda_permission" "point_query_permission" {
