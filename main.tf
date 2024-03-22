ee59c7a7fd569d17b5631590b34a1e5846990c78
GAPrado <guilhermeprado@outlook.com>
2024-03-22 14:39:23 -0300

    add report generate

--- main.tf
+++ main.tf
@@ -139,7 +139,7 @@ resource "aws_api_gateway_integration" "report_generate_integration" {
   }
 
   request_templates = {
-    "application/json" = "Action=SendMessage&MessageBody=\"{ \"header\": $input.params().header, \"body\": $input.body }\" "
+    "application/json" = "Action=SendMessage&MessageBody=\\"{ \\"header\\": \\"value\\", \\"body\\": \\"value body\\" }\\""
   }
 }
 
